18db0c9cc36458df9e8868e844dfce789ecab0bf
brian <kipkoechbrian0@gmail.com>
2022-11-26 17:44:41 +0300

    complete assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+    def index
+       items = Item.all
+       render json: items, only:[:id, :name, :description, :price], include: {user: {only: [:id, :username, :city]}}
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user, only: [:id, :username, :city], include: {items: {only: [:id, :name, :description, :price]}}
+    end
 end
